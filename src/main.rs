d208fb3744bdad1752bd9f8242587bf8147534f9
Leo Le Bouter <lle-bout@zaclys.net>
2019-06-30 15:27:53 +0200

    Blanket implement RPC methods and connect database

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use jsonrpc_derive::rpc;
 
 use serde_derive::{Deserialize, Serialize};
 
+use r2d2_postgres::{PostgresConnectionManager, TlsMode};
+
 #[derive(Serialize, Deserialize)]
 pub struct TaskId(u64);
 
@@ -14,18 +16,49 @@ pub trait Rpc {
     #[rpc(name = "create_profile")]
     fn create_profile(&self, base_name: String, name: String, yaml: String) -> Result<ProfileId>;
 
+    #[rpc(name = "list_profiles")]
+    fn list_profiles(&self, by_base: Option<Vec<String>>) -> Result<Vec<ProfileId>>;
+
+    #[rpc(name = "fetch_profile")]
+    fn fetch_profile(&self, id: ProfileId) -> Result<String>;
+
     #[rpc(name = "create_task")]
     fn create_task(&self, profile: ProfileId, file_name: String, data: Vec<u8>) -> Result<TaskId>;
+
+    #[rpc(name = "list_tasks")]
+    fn list_tasks(&self, by_profile: Option<Vec<ProfileId>>) -> Result<Vec<TaskId>>;
+
+    #[rpc(name = "fetch_task")]
+    fn fetch_task(&self, id: TaskId) -> Result<String>;
 }
 
-pub struct RpcImpl;
+pub struct RpcImpl {
+    db_pool: r2d2::Pool<PostgresConnectionManager>,
+}
+
 impl Rpc for RpcImpl {
+    fn create_profile(&self, base_name: String, name: String, yaml: String) -> Result<ProfileId> {
+        Ok(ProfileId(0))
+    }
+
+    fn list_profiles(&self, by_base: Option<Vec<String>>) -> Result<Vec<ProfileId>> {
+        Ok(vec![])
+    }
+
+    fn fetch_profile(&self, id: ProfileId) -> Result<String> {
+        Ok(String::new())
+    }
+
     fn create_task(&self, profile: ProfileId, file_name: String, data: Vec<u8>) -> Result<TaskId> {
         Ok(TaskId(0))
     }
 
-    fn create_profile(&self, base_name: String, name: String, yaml: String) -> Result<ProfileId> {
-        Ok(ProfileId(0))
+    fn list_tasks(&self, by_profile: Option<Vec<ProfileId>>) -> Result<Vec<TaskId>> {
+        Ok(vec![])
+    }
+
+    fn fetch_task(&self, id: TaskId) -> Result<String> {
+        Ok(String::new())
     }
 }
 
@@ -45,13 +78,22 @@ fn main() {
         )
         .get_matches();
 
-    let mut io = IoHandler::new();
-    io.extend_with(RpcImpl.to_delegate());
-
     let config_path = args.value_of("config").unwrap();
     let config: Config =
         serde_yaml::from_reader(std::fs::File::open(config_path).expect("could not open config"))
             .expect("could not parse config");
+
+    let mut io = IoHandler::new();
+
+    let db_pool = r2d2::Pool::new(
+        PostgresConnectionManager::new(config.postgres_uri, TlsMode::None)
+            .expect("could not create PostgresConnectionManager"),
+    )
+    .expect("could not create r2d2::Pool");
+
+    let rpc = RpcImpl { db_pool };
+
+    io.extend_with(rpc.to_delegate());
 
     let server = jsonrpc_tcp_server::ServerBuilder::new(io)
         .start(&std::net::SocketAddr::from((
